10d35fb3a7068e2ac528bf7582fbaea8faa814e3
Adrien POLY <adrienpoly@gmail.com>
2023-11-08 08:03:27 +0100

    specify the expires_in cache value

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
 
   # Use a different cache store in production.
-  config.cache_store = :litecache, {path: "./storage/cache.db"}
+  config.cache_store = :litecache, {path: "./storage/cache.db", expires_in: 1.day}
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
